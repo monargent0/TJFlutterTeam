14308f88f3dbd570e91934258637bf3f1d605a8c
jungeun <100196951+monargent0@users.noreply.github.com>
2022-05-30 18:44:34 +0900

    main update

--- mood_diary_app/lib/view/login.dart
+++ mood_diary_app/lib/view/login.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
-
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
