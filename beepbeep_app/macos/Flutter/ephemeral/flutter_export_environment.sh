1713ea1d66add8a85e265c960ae046cfc1d6e8ff
youngjin <daphnejin36@hanmail.net>
2022-06-17 18:23:26 +0900

    sanga
    1차 업뎃

---
+++ beepbeep_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tj/Documents/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Data/lastproject/FlutterTeam/beepbeep_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Volumes/Data/lastproject/FlutterTeam/beepbeep_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Volumes/Data/lastproject/FlutterTeam/beepbeep_app/.dart_tool/package_config.json"
