cc855bc8f13a13cdc15162c963fcefc98f364a79
dongwon <71756020+daviddev90@users.noreply.github.com>
2022-06-17 16:53:24 +0900

    로고 앱바 등등

---
+++ beepbeep_app/lib/chart/popChart.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PopChart extends StatefulWidget {
+  const PopChart({Key? key}) : super(key: key);
+
+  @override
+  State<PopChart> createState() => _PopChartState();
+}
+
+class _PopChartState extends State<PopChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
