5ee6f262c1b861dde42e079720c95271d115d035
SooyeonH <102337066+SooyeonH@users.noreply.github.com>
2022-06-17 17:47:45 +0900

    dday입력
    dday입력

--- beepbeep_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ beepbeep_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=/Users/hansooyeon/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/hansooyeon/Documents/GitHub/FlutterTeam/beepbeep_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
@@ -11,17 +10,3 @@ export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
-=======
-export "FLUTTER_ROOT=/Users/tj/Documents/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tj/Desktop/team_2/FlutterTeam/beepbeep_app"
-export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/tj/Desktop/team_2/FlutterTeam/beepbeep_app/lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
-export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/tj/Desktop/team_2/FlutterTeam/beepbeep_app/.dart_tool/package_config.json"
->>>>>>> origin/jjh
