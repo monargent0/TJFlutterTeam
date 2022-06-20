0eccdba9c69267b25741700c79e2a42450b5e1bb
jungeun <100196951+monargent0@users.noreply.github.com>
2022-06-20 12:23:46 +0900

    안쓰는파일삭제

--- beepbeep_app/lib/chart/chartRouter.dart
+++ beepbeep_app/lib/chart/chartRouter.dart
@@ -1,6 +1,5 @@
 import 'package:beepbeep_app/chart/chartPage.dart';
 import 'package:beepbeep_app/chart/chart_spop.dart';
-import 'package:beepbeep_app/chart/popChart.dart';
 import 'package:beepbeep_app/chart/totalChart.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +12,6 @@ class MyChart extends StatelessWidget {
       routes: {
         '/': (context) => const ChartPage(), // 차트 선택
         '/spop': (context) => const SpopChart(), // 인구수 그래프
-        '/pop': (context) => const PopChart(), // 동원 연습
         '/total':(context) => const TotalChart(),
       },
       initialRoute: '/',
