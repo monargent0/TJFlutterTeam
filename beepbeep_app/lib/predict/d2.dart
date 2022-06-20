8eacbc2ff660e95e5b6737e147a61ea1bc1c6bcd
jjhwnn <jeahwan174@gmail.com>
2022-06-20 15:52:41 +0900

    update ing...

--- beepbeep_app/lib/predict/d2.dart
+++ beepbeep_app/lib/predict/d2.dart
@@ -236,7 +236,7 @@ class _Dday2State extends State<Dday2> {
 
   insertAction() async {
     var url = Uri.parse(
-        'http://192.168.150.132:8080/Rserve/beep_predict_2.jsp?hdaytype=$hdaytype&hstart=$hstart&htraffic1=$htraffic1&htraffic2=$htraffic2&hspop=$hspop');
+        'http://localhost:8080/Rserve/beep_predict_2.jsp?hdaytype=$hdaytype&hstart=$hstart&htraffic1=$htraffic1&htraffic2=$htraffic2&hspop=$hspop');
     var response = await http.get(url);
     setState(() {
       var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
