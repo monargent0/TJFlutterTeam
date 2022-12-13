ca70c0e62bdc23eeb2b83e0d449da39dc2b8a8e4
monargent0 <owjddms1118@gmail.com>
2022-12-14 04:01:25 +0900

    시뮬레이터실행

--- mood_diary_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mood_diary_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
-export "FLUTTER_ROOT=/Users/tj/Documents/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tj/Desktop/team_2/FlutterTeam/mood_diary_app"
-=======
-export "FLUTTER_ROOT=/Volumes/Data/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Data/dongwon/FlutterTeam/mood_diary_app"
->>>>>>> cc855bc8f13a13cdc15162c963fcefc98f364a79
+export "FLUTTER_ROOT=/Users/ojeongeun/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ojeongeun/GitHub/TJFlutterTeam/mood_diary_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Volumes/Data/dongwon/FlutterTeam/mood_diary_app/lib/main.dart"
+export "FLUTTER_TARGET=/Users/ojeongeun/GitHub/TJFlutterTeam/mood_diary_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -16,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Volumes/Data/dongwon/FlutterTeam/mood_diary_app/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/ojeongeun/GitHub/TJFlutterTeam/mood_diary_app/.dart_tool/package_config.json"
